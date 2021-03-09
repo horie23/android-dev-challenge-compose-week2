831bf018e899ae4a51dddf798cd8d20d79a52c74
horie23 <horie@team-lab.com>
2021-03-10 00:10:10 +0900

    カウントダウンアニメーション追加

--- app/src/main/java/com/example/androiddevchallenge/ui/timer/CountdownScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/timer/CountdownScreen.kt
@@ -15,6 +15,14 @@
  */
 package com.example.androiddevchallenge.ui.timer
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -22,27 +30,35 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import org.intellij.lang.annotations.JdkConstants
+import com.example.androiddevchallenge.ui.theme.purple500
 
 @Composable
 fun CountdownScreen(viewModel: CountdownTimerViewModel = viewModel()) {
     val time: Long by viewModel.countdownTime.observeAsState(initial = 0L)
     val isCountingDown: Boolean by viewModel.isCountingDown.observeAsState(initial = false)
-
     Column(horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxWidth()) {
-        Countdown(time = time)
         CountdownSetting(isCountingDown = isCountingDown, onClick = { time ->
             if (isCountingDown) viewModel.stopCountdown() else viewModel.startCountdown(time)
         })
+        if (time > 0) {
+            Countdown(time = time)
+        }
     }
 }
 
@@ -68,10 +84,47 @@ fun CountdownSetting(isCountingDown: Boolean, onClick: (Long) -> Unit) {
 
 @Composable
 fun Countdown(time: Long) {
-    Text(
-        text = time.toString(),
-        fontSize = 48.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier.padding(48.dp)
+    Box(contentAlignment = Alignment.Center) {
+        CountdownCircle()
+        Text(
+            text = time.toString(),
+            fontSize = 64.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(48.dp)
+        )
+    }
+}
+
+@Composable
+fun CountdownCircle() {
+    val infiniteTransition = rememberInfiniteTransition()
+    val animationValue by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = FastOutSlowInEasing),
+            repeatMode = RepeatMode.Restart
+        )
     )
+    val stroke = with(LocalDensity.current) { Stroke(12.dp.toPx()) }
+    Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .aspectRatio(1f)
+            .padding(48.dp),
+        onDraw = {
+            val innerRadius = (size.minDimension - stroke.width) / 2
+            drawArc(
+                color = purple500,
+                startAngle = -90f,
+                sweepAngle = animationValue,
+                useCenter = false,
+                topLeft = Offset(
+                    (size / 2.0f).width - innerRadius,
+                    (size / 2.0f).height - innerRadius
+                ),
+                size = Size(innerRadius * 2, innerRadius * 2),
+                style = stroke,
+            )
+        })
 }
--- app/src/main/java/com/example/androiddevchallenge/ui/timer/CountdownTimerViewModel.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/timer/CountdownTimerViewModel.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.androiddevchallenge.ui.timer
 
 import androidx.lifecycle.LiveData
@@ -5,7 +20,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.ticker
@@ -41,6 +55,7 @@ class CountdownTimerViewModel : ViewModel() {
 
     fun stopCountdown() {
         _isCountingDown.value = false
+        _countdownTime.value = 0L
         countdownJob?.cancel()
     }
 }
